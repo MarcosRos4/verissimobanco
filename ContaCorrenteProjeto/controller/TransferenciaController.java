eebf4ec19ef04cc5f254231035dc1d4ebb80b8af
Marcos Vinicius Cavalcante Rosa <103190249+MarcosRos4@users.noreply.github.com>
2023-05-11 17:32:12 -0300

    Update TransferenciaController.java

--- ContaCorrenteProjeto/controller/TransferenciaController.java
+++ ContaCorrenteProjeto/controller/TransferenciaController.java
@@ -1,24 +1,25 @@
 package ContaCorrenteProjeto.controller;
 
+
 import ContaCorrenteProjeto.model.dao.TransferenciasDao;
 
 public class TransferenciaController {
     private String numero_da_conta;
     private TransferenciasDao transferencias = new TransferenciasDao();
-    private ContaController conta = new ContaController(this.numero_da_conta);
     
     public TransferenciaController(String numero_da_conta){
         this.numero_da_conta = numero_da_conta;
     }
 
     public void transferir(String conta_destino, String valor){
+        ContaController conta_origem_Controller = new ContaController(this.numero_da_conta);
         ContaController conta_destino_Controller = new ContaController(conta_destino);
-        if(this.conta.getSaldo() > Float.parseFloat(valor)){
-            this.conta.saque(Float.parseFloat(valor));
+        
+        if(conta_origem_Controller.saque(Float.parseFloat(valor))){
             conta_destino_Controller.deposito(Float.parseFloat(valor));
-            transferencias.criarTransferencia(conta.getNumero_da_conta(), conta_destino_Controller.getNumero_da_conta(), valor);
+            transferencias.criarTransferencia(this.numero_da_conta, conta_destino, valor);
+            System.out.println("Transferência realizada com sucesso. ");
         }
-        System.out.println("Transferência realizada com sucesso. ");
     }
 
     public void mostrarTransferencias() {
