75362a98a37b12eab23b79a81c0770ad448ed4e6
Marcos Vinicius Cavalcante Rosa <103190249+MarcosRos4@users.noreply.github.com>
2023-05-11 06:16:55 -0300

    Adição ver extrato e ciclo do aplicativo
    o aplicativo roda enquanto o usuário não terminar a execução do pragrama

--- ContaCorrenteProjeto/App.java
+++ ContaCorrenteProjeto/App.java
@@ -1,10 +1,13 @@
 package ContaCorrenteProjeto;
 import ContaCorrenteProjeto.view.AppView;
 import ContaCorrenteProjeto.controller.ContaController;
+import ContaCorrenteProjeto.controller.TransferenciaController;
 import ContaCorrenteProjeto.model.dao.ContasDao;
 public class App {
     // classe que faz as regras de negocio
     static ContaController contaController;
+
+    static TransferenciaController transferenciaController;
     // classe que altera o BD
     static ContasDao contasDao = new ContasDao();
     // classe que representa o UI
@@ -16,43 +19,68 @@ public class App {
         // o aplicativo começa com duas opções, ou logar com uma conta já existente ou criar uma nova conta
         interfacegrafica.imprimir("Deseja fazer Login(0) ou Cadastrar nova Conta(1)?\nResposta: ");
         respostaString = interfacegrafica.receberString();
-        // se escolher a opção 0 ele segue para o login e as opções de gerenciamento de conta
-        if(respostaString.equals("0")){
-            interfacegrafica.imprimir("Digite o seu numero_da_conta por favor: ");
-            respostaString = interfacegrafica.receberString();
-            // salva o numero da conta para poder acessar os gets e sets da conta e fazer alterações no BD
-            String numero_da_conta = respostaString;
-            try {
-                // intancia contaController com o numero_da_conta que vai ser acessada
-                contaController = new ContaController(respostaString);
-                interfacegrafica.imprimir(String.format("Conta do usuário %s acessada!\n",
-                contasDao.getNome(respostaString)));
-            } catch (Exception e) {
-                interfacegrafica.imprimir(e.getMessage());
+        while (true) {
+            // se escolher a opção 0 ele segue para o login e as opções de gerenciamento de conta
+            if(respostaString.equals("0")){
+                interfacegrafica.imprimir("Digite o seu numero_da_conta por favor: ");
+                respostaString = interfacegrafica.receberString();
+                while (contasDao.getNome(respostaString) == null) {
+                    interfacegrafica.imprimir("Digite o seu numero_da_conta por favor: ");
+                    respostaString = interfacegrafica.receberString();
+                }
+                
+                try {
+                    // intancia contaController com o numero_da_conta que vai ser acessada
+                    contaController = new ContaController(respostaString);
+                    transferenciaController = new TransferenciaController(respostaString);
+
+                    interfacegrafica.imprimir(String.format("Conta do usuário %s acessada!\n",
+                    contasDao.getNome(respostaString)));
+                
+                } catch (Exception e) {}
+                while (respostaString != "6") {
+                
+                    interfacegrafica.imprimir("O que deseja fazer?\nSaque(1) | Deposito(2) | Ver Saldo(3) | Ver Extrato(4) | Transferência(5) | Sair(6): ");
+                    respostaString = interfacegrafica.receberString();
+                    // se 1 -  saque
+                    if (respostaString.equals("1")) {
+                        interfacegrafica.imprimir("Digite o valor do saque: ");
+                        contaController.saque(interfacegrafica.receberFloat());
+                    }
+                    // se 2 - deposito
+                    else if(respostaString.equals("2")){
+                        interfacegrafica.imprimir("Digite o valor do depósito: ");
+                        contaController.deposito(interfacegrafica.receberFloat());
+                    }
+                    // se 3 - mostra o saldo atual
+                    else if(respostaString.equals("3")){
+                        interfacegrafica.imprimir(String.format("%s\n", contaController.verSaldo()));
+                    }
+                    // se 4 - mostra o histórico de transferências
+                    else if(respostaString.equals("4")){
+                        transferenciaController.mostrarTransferencias();
+                    }
+                    // se 5 - transferência
+                    else if (respostaString.equals("5")) {
+                        interfacegrafica.imprimir("Qual o valor a ser transferido?: ");
+                        Float valor =  interfacegrafica.receberFloat();
+                        interfacegrafica.imprimir("Qual a conta destino da transferência?: ");
+                        String conta_destino = interfacegrafica.receberString();
+                        transferenciaController.transferir(conta_destino, valor.toString());
+                    }
+                    else if (respostaString.equals("6")) {
+                        interfacegrafica.imprimir("Deseja fazer Login(0) ou Cadastrar nova Conta(1)?\nResposta: ");
+                        respostaString = interfacegrafica.receberString();
+                        break;
+                    }
+                    // se nenhuma das anteriores - mostra que a opção foi invalida e termina a execução do programa
+                    else{
+                        interfacegrafica.imprimir("Opção Inválida!");
+                    }
+                }
             }
-            interfacegrafica.imprimir("O que deseja fazer? Saque(1) ou Deposito(2) Ver Saldo(3): ");
-            respostaString = interfacegrafica.receberString();
-            // se 1 -  saque
-            if (respostaString.equals("1")) {
-                interfacegrafica.imprimir("Digite o valor do saque: ");
-                contaController.saque(interfacegrafica.receberFloat());
-            }
-            // se 2 - deposito
-            else if(respostaString.equals("2")){
-                interfacegrafica.imprimir("Digite o valor do depósito: ");
-                contaController.deposito(interfacegrafica.receberFloat());
-            }
-            // se 3 - mostra o saldo atual
-            else if(respostaString.equals("3")){
-                interfacegrafica.imprimir(String.format("%s", contasDao.getSaldo(numero_da_conta)));
-            }
-            // se nenhuma das anteriores - mostra que a opção foi invalida e termina a execução do programa
-            else{
-                interfacegrafica.imprimir("Opção Inválida!");
-            }
-        }
-        // se 1 - cadastra uma nova conta
-        else if(respostaString.equals("1")){
+            // se 1 - cadastra uma nova conta
+            else if(respostaString.equals("1")){
             interfacegrafica.imprimir("Para cadastrar uma nova conta digite seu NOME e NUMERO_DA_AGENCIA."+
             "\nAgencias disponíveis: 1111 e 1112"+
             "\nNome: ");
@@ -60,10 +88,16 @@ public class App {
             interfacegrafica.imprimir("Agencia Escolhida: ");
             String numero_da_agencia = interfacegrafica.receberString();
             contasDao.inserirContas(nome, numero_da_agencia);
+            interfacegrafica.imprimir("Deseja fazer Login(0) ou Cadastrar nova Conta(1)?\nResposta: ");
+            respostaString = interfacegrafica.receberString();
         }
-        // se nenhuma das anteriores - mostra que a opção foi invalida e termina a execução do programa
-        else{
-            interfacegrafica.imprimir("Opção inválida!");
+            // se nenhuma das anteriores - mostra que a opção foi invalida e termina a execução do programa
+            else{
+            interfacegrafica.imprimir("Opção inválida!\n");
+            interfacegrafica.imprimir("Deseja fazer Login(0) ou Cadastrar nova Conta(1)?\nResposta: ");
+            respostaString = interfacegrafica.receberString();
+        }
+        
         }
     }
 }
