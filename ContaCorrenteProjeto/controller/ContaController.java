04055c3687855f3c4224e8607707eb9c386e5699
Marcos Vinicius Cavalcante Rosa <103190249+MarcosRos4@users.noreply.github.com>
2023-05-11 06:19:52 -0300

    Update ContaController.java

--- ContaCorrenteProjeto/controller/ContaController.java
+++ ContaCorrenteProjeto/controller/ContaController.java
@@ -5,30 +5,43 @@ import ContaCorrenteProjeto.model.dao.ContasDao;
 public class ContaController {
     // numero da conta que vai ser acessada
     private String numero_da_conta;
-    ContasDao conta = new ContasDao();
+    private ContasDao conta = new ContasDao();
     // construtor do objeto já define a conta acessada na instanciação
     public ContaController(String numero_da_conta){
         this.numero_da_conta = numero_da_conta;   
     }
     // recebe um valor float para sacar
-    public void saque(float valor){
+    public boolean saque(float valor){
         // se o valor for menor que o saldo disponível ele executa o saque
-        if (conta.getSaldo(this.numero_da_conta) > valor) {
-            conta.setSaldo(this.numero_da_conta, String.format("%s",(conta.getSaldo(this.numero_da_conta) - valor)));
+        if (this.conta.getSaldo(this.numero_da_conta) > valor) {
+            this.conta.setSaldo(this.numero_da_conta, String.format("%s",(this.conta.getSaldo(this.numero_da_conta) - valor)));
             System.out.println("Saque Concluido");
+            return true;
         }
         // se não ele imprime que o saldo foi insuficiente
         else{
             System.out.println("Saldo insuficiente");
+            return false;
         }
     }
     // recebe um valor para depositar
     public void deposito(float valor){
         // soma o saldo atual ao valor passado
-        conta.setSaldo(this.numero_da_conta, String.format("%s",
-        (conta.getSaldo(this.numero_da_conta) + valor)));
+        this.conta.setSaldo(this.numero_da_conta, String.format("%s",
+        (this.conta.getSaldo(this.numero_da_conta) + valor)));
         System.out.println("Depósito Concluido");
     }
+
+    public String getNumero_da_conta() {
+        return this.numero_da_conta;
+    }
+
+    public String verSaldo() {
+        return String.format("Saldo atual R$: %s", this.conta.getSaldo(this.numero_da_conta));
+    }
     
+    public float getSaldo(){
+        return this.conta.getSaldo(numero_da_conta);
+    }
 
 }
